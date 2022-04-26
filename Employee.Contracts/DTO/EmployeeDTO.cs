7599102016d58d3dfb3bcb41173fbbc27792c78f
Riyaz0101 <102675231+Riyaz0101@users.noreply.github.com>
2022-04-26 12:54:27 +0530

    Add Contracts

---
+++ Employee.Contracts/DTO/EmployeeDTO.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Employee.Contracts.DTO
+{
+    public class EmployeeDTO
+    {
+        [Required]
+        public int EmployeeId { get; set; }
+        [Required]
+        public string EmployeeName { get; set; }
+        [Required]
+        public string EmployeeAddress { get; set; }
+    }
+}
---
+++ Employee.Contracts/Models/ServiceUnavailableException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Contracts.Models
+{
+    public class ServiceUnavailableException : Exception
+    {
+        public ServiceUnavailableException()
+        {
+        }
+
+        public ServiceUnavailableException(string message) : base(message)
+        {
+        }
+
+        public ServiceUnavailableException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ServiceUnavailableException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
