cfc9c6c1f73c1e2e34d22d8cee29757361fc43dd
Riyaz0101 <102675231+Riyaz0101@users.noreply.github.com>
2022-04-26 12:55:18 +0530

    Add DataAccess

---
+++ Employee.DataAccess/GenericRepository/GenericRepository.cs
@@ -0,0 +1,110 @@
+﻿using Employee.DataAccess.GenericRepository.Interfaces;
+using Employee.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.DataAccess.GenericRepository
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        internal EmployeeContext context;
+        internal DbSet<TEntity> dbSet;
+        public GenericRepository(EmployeeContext context)
+        {
+            this.context = context;
+            dbSet = context.Set<TEntity>();
+        }
+
+        public void Delete(object id)
+        {
+            TEntity entityToDelete = dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+        public virtual void Delete(TEntity entityToDelete)
+        {
+            if (context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                dbSet.Attach(entityToDelete);
+            }
+            dbSet.Remove(entityToDelete);
+        }
+        public IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "")
+        {
+            IQueryable<TEntity> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            foreach (string includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "")
+        {
+            IQueryable<TEntity> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (string includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return await orderBy(query).ToListAsync();
+            }
+            else
+            {
+                return await query.ToListAsync();
+            }
+        }
+
+        public TEntity GetByID(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public async Task<TEntity> InsertAsync(TEntity entity)
+        {
+            EntityEntry<TEntity> insert = await dbSet.AddAsync(entity);
+            return insert.Entity;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public async Task SaveAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+
+        public TEntity Update(TEntity entityToUpdate)
+        {
+            context.Update(entityToUpdate);
+            return entityToUpdate;
+        }
+    }
+}
---
+++ Employee.DataAccess/GenericRepository/Interfaces/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.DataAccess.GenericRepository.Interfaces
+{
+    public interface IGenericRepository<TEntity>
+    {
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "");
+        Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "");
+        Task<TEntity> InsertAsync(TEntity entity);
+        TEntity GetByID(object id);
+        TEntity Update(TEntity entityToUpdate);
+        void Delete(object id);
+        void Save();
+        Task SaveAsync();
+    }
+}
